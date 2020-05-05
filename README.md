317e569ae2469c3e06b3091ebf485723f7775fcb
ahmedkidwai <umkidwaa@myumanitoba.ca>
2020-05-05 10:19:01 -0500

    Created Version 1 of ZST

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Sales-Page-Tracker
-Sales Page Tracker
+# Zumies Sales Tracker
+
+> Easy to use sales page tracker for the Zumies e-commerce store
+
+> Created because I got tired of missing sales on longboards/cruisers 🛹.
+
+### Setup
+
+The Zumies Sale Tracker (ZST) requires users email credentials to send off emails. ZST checks environement variables for email addresses and passwords. App specific passwords are still recommended - Instructions for [Gmail Users](https://support.google.com/accounts/answer/185833?hl=en)
+
+#### Shell Profile Updates
+Add the following lines to your .zshrc or .bash_profile
+```
+export EMAIL="email@email.com"
+```
+```
+export PASSWORD="app-specific-password"
+```
+Restart your shell after updating.
+
+### Running ZST
+Navigate to the src folder and run
+`$  python3 Zumies_Sale_Tracker`
+Fill in the required prompts.
+>Product Name: This is an identifier used to email the user a product SKU has been found.
+
+>SKU: The SKU is the unique product identifer the application uses to check if a product is on sale.
+
+> Sales Page URL: Navigate to the section your product is found (example: Sale > Skateboards > Cruisers Completes) and copy the URL. The application will scan this page for products.
+
+> Email: This is the email you want to receive updates to.
---
+++ src/Zumies_Sale_Tracker.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import smtplib
+from email.message import EmailMessage
+import os
+
+
+def main():
+    product_identifier = input(
+        "What is the name of the product you are looking for: ")
+    sku = input("What is the product's SKU: ")
+    url = input("Enter the sales page you would like monitored: ")
+    receiver = input(
+        "What email do you want to be alerted on when a product is found: ")
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) ' +
+        'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+        'Chrome/39.0.2171.95 Safari/537.36'}
+    response = requests.get(url, headers=headers).text
+    soup = BeautifulSoup(response, 'html.parser')
+    analyzeData(soup, product_identifier, receiver, sku)
+
+
+def analyzeData(soup, product_identifier, receiver, sku):
+    found = False
+
+    while not found:
+        if str(soup).find(sku) > -1:
+            email(receiver, product_identifier)
+            found = True
+        else:
+            time.sleep(600)
+            continue
+
+
+def email(receiver, product_identifier):
+
+    sender = 'ahmedbot@salestracker.com'
+    msg = EmailMessage()
+
+    msg.set_content("Zumies Sale Tracker here with some good news! " +
+                    product_identifier + " is now on the sales page. Get it ASAP!")
+
+    msg['Subject'] = 'Sale Update: ' + product_identifier + " Now on Sale!"
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(os.environ.get('EMAIL'), os.environ.get('PASSWORD'))
+    server.send_message(msg)
+    server.quit()
+
+
+if __name__ == "__main__":
+    main()
